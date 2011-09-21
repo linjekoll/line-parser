01de1e0b3596518c8af8c0e03b3ee18d0789e3a7
Linus Oleander <linus@oleander.nu>
2011-09-21 19:32:45 +0200

    A trip id should not be added

--- lib/station.rb
+++ lib/station.rb
@@ -1,29 +1,47 @@
+# -*- encoding : utf-8 -*-
 require "update"
 require "base"
 
 module LinearT
   class Station < LinearT::Base
+    attr_reader :trip_ids
     # @channel EM.Channel Object
     # @id Station id
-    # @
-    def initialize(channel, id)
-      @travel_times = {}
+    # @travel_times {
+    #   "4" => {
+    #     "Mölndal" => 60,
+    #     "Angered" => 80
+    #   }
+    # }
+    
+    def initialize(channel, id, travel_times = {})
+      @travel_times = {}.merge(travel_times)
       @start_time   = {}
       @stations     = {}
-      @trip_id      = [] # A list of nearby trains
+      @trip_ids     = [] # A list of nearby trains
       @channel      = channel # EM channel
       @id           = id # Station id
     end
     
-    def update!(trip_id)
+    def update!(ingoing_trip_id)
       url = %w{
         http://vasttrafik.se/External_Services/NextTrip.asmx/GetForecast?
         identifier=%s&
         stopId=%s
       }.join % [api_key, @id]
-      
-      download!(url).css("forecast items item").each do |stop|
-        puts "It's %s seconds left for line %s.".green % [Time.parse(stop.attr("next_trip_forecast_time")).to_i - Time.now.to_i, stop.attr("line_id")]
+
+      download!(url).css("forecast items item").each do |stop|      
+        diff        = Time.parse(stop.attr("next_trip_forecast_time")).to_i - Time.now.to_i
+        destination = stop.at_css("destination").content
+        trip_id     = stop.attr("trip_id")
+        line        = stop.attr("line_id")
+        
+        # Is the tram nearby?
+        if dest = @travel_times[line] and time = dest[destination] and diff < time and diff > 0
+          @trip_ids.push(trip_id)
+        else
+          @trip_ids.delete(trip_id)
+        end
       end
     end
   end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,13 +1,28 @@
+# -*- encoding : utf-8 -*-
 describe LinearT::Station do
   describe "simple" do
     use_vcr_cassette "00003980"
     before(:each) do
-      @station = LinearT::Station.new(nil, "00003980")
+      @travel_times = {
+        "4" => {
+          "Mölndal" => 360,
+          "Angered" => 250
+        }
+      }
+      @station = LinearT::Station.new(nil, "00003980", @travel_times)
     end
     
-    it "should do something" do
+    it "should add a trip id" do
       Timecop.travel(Time.parse("21 Sep 2011 16:10:37 GMT")) do
         @station.update!(nil)
+        @station.trip_ids.should include("30810")
+      end
+    end
+    
+    it "should NOT add a trip id" do
+      Timecop.travel(Time.parse("22 Sep 2011 16:10:37 GMT")) do
+        @station.update!(nil)
+        @station.trip_ids.should_not include("30810")
       end
     end
   end
