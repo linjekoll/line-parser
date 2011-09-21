a7bbe8116f4b4100dcdb72904959b94d7fb1869a
Linus Oleander <linus@oleander.nu>
2011-09-21 19:37:55 +0200

    should be able to remove trip id

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,17 +1,17 @@
 # -*- encoding : utf-8 -*-
 describe LinearT::Station do
-  describe "simple" do
-    use_vcr_cassette "00003980"
-    before(:each) do
-      @travel_times = {
-        "4" => {
-          "Mölndal" => 360,
-          "Angered" => 250
-        }
+  use_vcr_cassette "00003980"
+  before(:each) do
+    @travel_times = {
+      "4" => {
+        "Mölndal" => 360,
+        "Angered" => 250
       }
-      @station = LinearT::Station.new(nil, "00003980", @travel_times)
-    end
-    
+    }
+    @station = LinearT::Station.new(nil, "00003980", @travel_times)
+  end
+  
+  describe "trip id" do    
     it "should add a trip id" do
       Timecop.travel(Time.parse("21 Sep 2011 16:10:37 GMT")) do
         @station.update!(nil)
@@ -21,6 +21,17 @@ describe LinearT::Station do
     
     it "should NOT add a trip id" do
       Timecop.travel(Time.parse("22 Sep 2011 16:10:37 GMT")) do
+        @station.update!(nil)
+        @station.trip_ids.should_not include("30810")
+      end
+    end
+    
+    it "should remove trip id if trap has passed the current station" do
+      Timecop.travel(Time.parse("21 Sep 2011 16:10:37 GMT")) do
+        @station.update!(nil)
+      end
+      
+      Timecop.travel(Time.parse("21 Sep 2011 16:30:37 GMT")) do
         @station.update!(nil)
         @station.trip_ids.should_not include("30810")
       end
