9b9536c46135d5f7d72829c9e6c0955454ae464f
Linus Oleander <linus@oleander.nu>
2011-09-26 00:56:00 +0200

    Using brute force when selecting sample station, 4 in this case

--- server.rb
+++ server.rb
@@ -4,11 +4,6 @@ require_relative "lib/line_populator"
 require_relative "lib/station"
 
 EM.run do
-  worker_channel = EM::Channel.new
-  worker_channel.subscribe do |params|
-    puts "Params: #{params}".yellow
-  end
-  
   line_populator = LinearT::LinePopulator.new("00012110", "00001075")
   
   stations = line_populator.stations
@@ -20,14 +15,14 @@ EM.run do
   
   stations.each_with_index do |s, index|
     station = s.station
-        
-    previous = station[:before] || {}
+  
+    before = station[:before] || {}
     after = station[:after] || {}
     
     s.line = "4"
     
     s.travel_times = {
-      line_populator.start => previous[:time],
+      line_populator.start => before[:time],
       line_populator.stop => after[:time]
     }
     
@@ -40,21 +35,15 @@ EM.run do
     new_stations << s
   end
   
-  station = stations[4]
-  w = station.departures.reject do |dep|
-    not dep[:line] == "4" 
-  end
-  
-  trip_id = w.first[:trip_id]
-  
-  station.update!(trip_id)
-  
+  departure = nil
+  station = nil
+  stations.each do |station|
+    departure = station.departures.reject do |dep|
+      not dep[:line] == "4" 
+    end.first
+    
+    unless departure.nil?
+      station.update!(departure[:trip_id]); break
+    end
+  end    
 end
-
-# ,
-# stations: {
-#   station[:line] => {
-#     before[:name] => stations[index - 1],
-#     after[:name] => stations[index + 1]
-#   }
-# },
